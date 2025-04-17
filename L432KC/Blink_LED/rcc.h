9ade1b19c77dfd5eaa23a632a95576522be95ef1
Dimitry Jean-Laurent <d_jeanlaurent@apple.com>
2025-04-17 16:50:27 -0700

    clean up

--- L432KC/Blink_LED/rcc.h
+++ L432KC/Blink_LED/rcc.h
@@ -6,9 +6,9 @@
 typedef struct
 {
 	volatile uint32_t DUMMY[19];  // 76 byte pad
-	volatile uint32_t AHB2_ENR;   // Offset 0x4C  
-    volatile uint32_t DUMMY2[2];  // 8 byte pad
-	volatile uint32_t APB1_ENR;	  // Offset 0x58
+	volatile uint32_t AHB2_ENR;   // Offset 0x4C
+	volatile uint32_t DUMMY2[2];  // 8 byte pad
+	volatile uint32_t APB1_ENR;   // Offset 0x58
 } RCC_TypeDef;
 
 #endif 

ac07186f2ca2f3064ace0aae5631ea88f8a0d5a7
Dimitry Jean-Laurent <d_jeanlaurent@apple.com>
2025-04-19 18:11:58 -0700

    GOT CMSIS TO WORKgit add .git add .

--- L432KC/UART_Print_CMSIS/gpio.h
+++ L432KC/UART_Print_CMSIS/gpio.h
@@ -1,23 +1,10 @@
 #include <stdint.h> 
+#include "stm32l432xx.h"
 
 #ifndef GPIO_H
 #define GPIO_H
 
-typedef struct{
-	volatile uint32_t MODER;    // offset: 0x0
-	volatile uint32_t OTYPER;   // offset: 0x4
-	volatile uint32_t OSPEEDR;  // offset: 0x8 
-	volatile uint32_t PUPDR;    // offset: 0xC 
-	volatile uint32_t IDR;		// offset: 0x10 
-	volatile uint32_t ODR;		// offset: 0x14 
-	volatile uint32_t BSRR;		// offset: 0x18 
-	volatile uint32_t LCKR;     // offset: 0x1C
-	volatile uint32_t AFRL;     // ofsett: 0x20
-	volatile uint32_t AFRH;     // offset: 0x24
-	volatile uint32_t BRR;      // offset: 0x28
-
-} GPIO_TypeDef;
-
+void init_gpio();
 
 
 
--- L432KC/UART_Print_CMSIS/main.c
+++ L432KC/UART_Print_CMSIS/main.c
@@ -1,21 +1,20 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "base.h"
 #include "gpio.h"
-#include "rcc.h"
-#include "timer.h"
+#include "gpio.h"
 #include "nvic.h"
-#include "usart.h"
 
-#define GPIO_B  ((GPIO_TypeDef *) GPIOB_BASE)
+#if 0
 #define GPIO_A  ((GPIO_TypeDef *) GPIOA_BASE)
 #define RCC     ((RCC_TypeDef *) RCC_BASE)
 #define USART2 ((USART_TypeDef*) USART2_BASE)
 #define TIM2 ((TIMER_TypeDef *) TIM2_BASE)
+#endif
 #define TIM2_IRQn 28
 
 
+#if 0
 void init_gpio(){
 	RCC->AHB2_ENR |= (1U << 1);          // Enable GPIO A Clock
 	GPIO_B->OSPEEDR &= ~(3U << (3 *2));  // Clear the bit
@@ -25,23 +24,23 @@ void init_gpio(){
 	
 
 }
-
+#endif
 //PA2 set to AF7 for USART 2 TX
 void init_usart(){
 
 	// Enable RCC GPIO A
-	RCC->AHB2_ENR |= (1U << 0);
+	RCC->AHB2ENR |= (1U << 0);
 
 	// Clear + Set PA2 to Alternate Function Mode
-	GPIO_A->MODER &= ~(3U << (2 * 2));
-	GPIO_A->MODER |= (2U << (2 * 2));
+	GPIOA->MODER &= ~(3U << (2 * 2));
+	GPIOA->MODER |= (2U << (2 * 2));
      
 	// Clear + Set PA2 Alternate Function to AF7
-	GPIO_A->AFRL &= ~(15U << (2 * 4));
-	GPIO_A->AFRL |=  (7U << (2 * 4));
+	GPIOA->AFR[0] &= ~(15U << (2 * 4));
+	GPIOA->AFR[0] |=  (7U << (2 * 4));
 
 	// Enable USART2 RCC 
-	RCC->APB1_ENR |= (1U << 17);
+	RCC->APB1ENR1 |= (1U << 17);
 			
 	// Configure Baud Rate... for some reason we only see output when terminal baud is 14400
 	// Also I am pretty sure the default clock is 4Mhz, need to investigate.... but it works!
@@ -83,19 +82,19 @@ void init_tim2(){
 	 * Goal UEV of 1 Hz
 	 * Bus_Clock = APB1_Bus Freq = 16Mhz
 	 * 
-	 * To achieve 1Hz we Prescaler = 15999, Period = 999
+	 * To achieve 1Hz we Prescaler = 3999, Period = 999
 	 *
-	 * 16,000,000 / ((15999 + 1)(999+1)) = 1 Hz
+	 * 4,000,000 / ((3999 + 1)(999+1)) = 1 Hz
 	 */
 
 	// Enable Clock for TIM2
-	RCC->APB1_ENR |= 0x00000001;
+	RCC->APB1ENR1 |= 0x00000001;
 	
 	// Set Auto Reload Register 
 	TIM2->ARR = 999;
 
 	// Set Prescaler 
-	TIM2->PSC = 15999;
+	TIM2->PSC = 3999;
 	
 	// Set TIM2 COUNT
 	TIM2->CNT = 0;
@@ -110,7 +109,7 @@ void init_tim2(){
 void Tim2_Handler(void){
 	
 	// Toggle the LED
-	GPIO_B->ODR ^= (1U << 3);
+	GPIOB->ODR ^= (1U << 3);
 		
 	// Clear Update interrupt pending flag
 	if (TIM2->SR & 0x00000001) {
@@ -138,16 +137,18 @@ int main(){
 	init_usart();
 
 	// Toggle GPIO B 3
-	GPIO_B->ODR |= (1U << 3);
+	GPIOB->ODR |= (1U << 3);
 	
 	// Enable Tim2 in NVIC
 	enable_interrupt(TIM2_IRQn);
 	
 	while(1) {
-	    char * str = "Hello World\n";
+
+		char * str = "Hello World\n";
 		while(*str){
 			put_char_usart(*str++);	
 		}
+
 	}
 	
 	// Disable Tim2
